b365f9e86238d2387ff8582c99219c92fc327bf2
dev-vin-dev <vincentmusyimi40@gmail.com>
2025-06-27 14:53:03 +0300

    Creation of the dynamic quote page

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -60,7 +60,33 @@ function addQuote () {
     alert("Quote added successfully")
 }
 
-newQuoteBtn.addEventListener("click", showRandomQuote);
+function createAddQuoteForm() {
+  const formContainer = document.createElement("div");
+
+  const quoteInput = document.createElement("input");
+  quoteInput.id = "newQuoteText";
+  quoteInput.type = "text";
+  quoteInput.placeholder = "Enter a new quote";
+
+  const categoryInput = document.createElement("input");
+  categoryInput.id = "newQuoteCategory";
+  categoryInput.type = "text";
+  categoryInput.placeholder = "Enter quote category";
+
+  const addButton = document.createElement("button");
+  addButton.textContent = "Add Quote";
+  addButton.onclick = addQuote;
+
+  formContainer.appendChild(quoteInput);
+  formContainer.appendChild(categoryInput);
+  formContainer.appendChild(addButton);
+
+  document.body.appendChild(formContainer);
+}
+
+createAddQuoteForm();
+
+newQuoteBtn.addEventListener("click", displayRandomQuote);
 
 //Populate categories on initial load
 populateCategories();
